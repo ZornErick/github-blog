74882b08f201e94b86982825ccdb3edfa6bd5609
erickzorn <erick.nogueira.zorn@gmail.com>
2024-07-03 20:04:46 -0300

    Configuração dos estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import {ThemeProvider} from "styled-components";
+import {GlobalStyle} from "./styles/global.ts";
+import {defaultTheme} from "./styles/themes/default.ts";
+
 function App() {
   return (
-    <>
-    </>
-  )
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyle />
+      </ThemeProvider>
+  );
 }
 
 export default App;
